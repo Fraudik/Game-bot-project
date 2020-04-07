46f66b81ac0c8c082f442e865839b3a5497122ec
Fraudik <52631426+Fraudik@users.noreply.github.com>
2020-04-07 14:17:53 +0200

    Add files via upload
    Сформирован общий план бота, начата постройка диалоговой системы. Добавлены названия будущих игр.

--- school.py
+++ school.py
@@ -2,6 +2,24 @@ from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext import CommandHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+# это все игры, которые доступны, в формате: команду на вызов игры - описание игры
+games = '/быки и коровы - математическая игра, в двух изданиях: в одиночку и против бота\n' \
+        '/крестики-нолики - классические крестики-нолики с 3 уровнями сложности\n' \
+        '/сапер - классический сапер, размер поля варьируется от 5 на 5, до 26 на 26 клеток\n' \
+        '/камень-ножницы-бумага - классические... камень-ножницы-бумага!\n' \
+        '/кости - вы должны будете выбросить кости больше чем у вашего противника - бота\n\n' \
+        'Более подробные правила игр описаны внутри каждой из них. Пусть Фортуна будет благосклонна к вам!'
+# это база откуда мы будем брать реакции на разные фразы.
+dialog_base = {['игр', 'play', 'game']: 'Вот список моих игр: ' + games}
+greetings = 'хай хэй здравствуй прив hi hello hey'.split(' ')
+for i in greetings:
+    dialog_base[i] = i.capitalize() + ', как дела?'
+good_emotions = 'нормально хорошо круто весело потрясно неплохо отлично'.split()
+for i in good_emotions:
+    dialog_base[i] = 'Рад за тебя! А знаешь, что может сделать этот день еще лучше? Мои игры!'
+bad_emotions = 'грустно скучно плохо ужасно скучно'.split()
+for i in bad_emotions:
+    dialog_base[i] = 'Ох, мне жаль. Но я могу попробовать немного развеселить тебя играми.'
 
 
 def start(update, context):
